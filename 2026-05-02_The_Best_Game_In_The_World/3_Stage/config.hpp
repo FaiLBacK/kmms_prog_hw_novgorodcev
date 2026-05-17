10d72282903f30261f8d7bf618f2add02bfe8d05
FaiLBacK <novsevpav@gmail.com>
2026-05-17 19:37:10 +0300

    napisal entity i nachap zapolnenie config-const peremennie

---
+++ 2026-05-02_The_Best_Game_In_The_World/3_Stage/config.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace Config {
+    constexpr float GRAVITY = 980.0f;
+    constexpr float FRICTION = 0.9f;
+    constexpr float GROUND_Y = 500.0f;
+    constexpr int BRICK_INITIAL_HP = 2;
+    constexpr int INDESTRUCTIBLE_HP = 999;
+}
