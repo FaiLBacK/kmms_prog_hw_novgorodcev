ab8a1fc67d26e532c4801d896106ba331645992d
FaiLBacK <novsevpav@gmail.com>
2026-05-20 21:17:42 +0300

    player.cpp and enemy.cpp is done

---
+++ 2026-05-02_The_Best_Game_In_The_World/3_Stage/src/enemy.cpp
@@ -0,0 +1,67 @@
+#include <cmath>
+
+#include "enemy.hpp"
+
+Enemy::Enemy(float xPos, float yPos)
+    : xPos(xPos)
+    , yPos(yPos)
+    , speed(ENEMY_SPEED)
+    , width(ENEMY_WIDTH)
+    , height(ENEMY_HEIGHT)
+{
+}
+
+Bounds Enemy::getBounds() const
+{
+    Bounds bounds;
+    bounds.xPos = xPos;
+    bounds.yPos = yPos;
+    bounds.width = width;
+    bounds.height = height;
+    return bounds;
+}
+
+char Enemy::getType() const
+{
+    return TYPE_ENEMY;
+}
+
+void Enemy::reverseDirection()
+{
+    speed = -speed;
+}
+
+void Enemy::render(char buffer[MAP_HEIGHT][MAP_WIDTH + 1], float cameraX) const
+{
+    const int32_t drawX = static_cast<int32_t>(std::round(xPos - cameraX));
+    const int32_t drawY = static_cast<int32_t>(std::round(yPos));
+    const int32_t objW = static_cast<int32_t>(std::round(width));
+    const int32_t objH = static_cast<int32_t>(std::round(height));
+
+    for (int32_t col = drawX; col < (drawX + objW); col++)
+    {
+        for (int32_t row = drawY; row < (drawY + objH); row++)
+        {
+            const bool isValidX = (col >= 0 && col < MAP_WIDTH);
+            const bool isValidY = (row >= 0 && row < MAP_HEIGHT);
+
+            if (isValidX == true && isValidY == true && row > 1)
+            {
+                buffer[row][col] = getType();
+            }
+        }
+    }
+}
+
+void Enemy::update(float dt)
+{
+    xPos += speed * dt;
+
+    const bool hitLeftBorder = (xPos < 0.0f);
+    const bool hitRightBorder = (xPos > static_cast<float>(MAP_WIDTH) * 2.0f);
+
+    if (hitLeftBorder == true || hitRightBorder == true)
+    {
+        reverseDirection();
+    }
+}
