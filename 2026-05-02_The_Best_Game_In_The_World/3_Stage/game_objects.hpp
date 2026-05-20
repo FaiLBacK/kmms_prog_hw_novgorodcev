6acd3ea0c036ec0dcfa71427cbbbbe8da6e43de7
FaiLBacK <novsevpav@gmail.com>
2026-05-20 19:04:01 +0300

    sdelal game interface object's and player-class

---
+++ 2026-05-02_The_Best_Game_In_The_World/3_Stage/game_objects.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "bounds.hpp"
+
+class GameObject
+{
+public:
+    virtual ~GameObject() = default;
+    virtual void update(float dt) = 0;
+    virtual void render(char buffer[MAP_HEIGHT][MAP_WIDTH + 1], float cameraX) const = 0;
+    virtual Bounds getBounds() const = 0;
+    virtual char getType() const = 0;
+};
